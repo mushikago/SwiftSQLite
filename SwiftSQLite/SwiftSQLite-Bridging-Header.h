fba11cbbebc63c88b1a103a139531bd87f0cdeaa
mushikago <tshiraishi@Tetsuya-no-MacBook-Air.local>
2014-08-12 14:30:35 +0900

    1st commit

---
+++ SwiftSQLite/SwiftSQLite-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  SwiftSQLite-Bridging-Header.h
+//  SwiftSQLite
+//
+//  Created by Tetsuya Shiraishi on 2014/08/12.
+//  Copyright (c) 2014年 mushikago. All rights reserved.
+//
+
+#import <sqlite3.h>
+#import "FMDatabase.h"
+
+#ifndef SwiftSQLite_SwiftSQLite_Bridging_Header_h
+#define SwiftSQLite_SwiftSQLite_Bridging_Header_h
+
+
+#endif
--- SwiftSQLite/ViewController.swift
+++ SwiftSQLite/ViewController.swift
@@ -20,6 +20,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pushButton(sender: AnyObject) {
+        println("push")
+        let _dbfile:NSString = "sqlite.db"
+        let _dir:AnyObject = NSSearchPathForDirectoriesInDomains(
+            NSSearchPathDirectory.DocumentDirectory,
+            NSSearchPathDomainMask.UserDomainMask,
+            true)[0]
+        let fileManager:NSFileManager = NSFileManager.defaultManager()
+        let _path:String = _dir.stringByAppendingPathComponent(_dbfile)
+        
+        println(_path)
+        
+        if(!fileManager.fileExistsAtPath(_path)){
+            //ファイルがない場合はDBファイル作成（最初の押下）
+            let _db = FMDatabase(path: _path)
+            let _sql = "CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT);"
+            
+            _db.open()
+            
+            var _result = _db.executeStatements(_sql)
+            println(_result)
+            
+            _db.close()
+            
+        }else{
+            //ファイルがすでに存在する場合（２回目以降の押下）
+            let _db = FMDatabase(path: _path)
+            
+            let _sql_insert = "insert into test (title) values (?);"
+            let _sql_select = "SELECT title FROM test WHERE id = ?"
+            
+            
+            _db.open()
+            
+            var _result_insert = _db.executeUpdate(_sql_insert, withArgumentsInArray: ["あいうえお"])
+            var _rows = _db.executeQuery(_sql_select, withArgumentsInArray: [1])
+            
+            
+            while(_rows.next()){
+                var _title = _rows.stringForColumn("title")
+                println(_title)
+            }
+            
+            _db.close()
+        }
+    }
 
 }
 
